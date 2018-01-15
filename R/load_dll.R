efb181c7484b5cb5375936b9d8764d5097978e6a
frumentum <simon-poeschl@posteo.de>
2018-01-15 11:03:45 +0100

    version 0.2.1
    only source code available in package - no more dll

--- R/load_dll.R
+++ R/load_dll.R
@@ -1,10 +1,11 @@
 #' Load BDAT Dynamically Linked Libraries
 #'
 #' @return Invisibly NULL.
+#' @useDynLib rBDATPRO
 #'@export
 load_dll <- function() {
-    dyn.load(system.file("dll", "DFORRT.dll", package = "rBDATPRO"))
-    dyn.load(system.file("dll", "BDATPRO.dll", package = "rBDATPRO"))
-    dyn.load(system.file("dll", "BDATPRO_R.dll", package = "rBDATPRO"))
-    return(invisible(NULL))
+    # dyn.load(system.file("dll", "DFORRT.dll", package = "rBDATPRO"))
+    # dyn.load(system.file("dll", "BDATPRO.dll", package = "rBDATPRO"))
+    # dyn.load(system.file("dll", "BDATPRO_R.dll", package = "rBDATPRO"))
+    # return(invisible(NULL))
 }
--- devel.R
+++
@@ -1,7 +0,0 @@
-if (FALSE) {
-    devtools::check()
-    devtools::load_all()
-    loadBDAT("BDATORHX")
-}
-devtools::install()
-rBDATPRO::loadBDAT("BDATORHX")
